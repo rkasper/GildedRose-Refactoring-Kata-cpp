375bcae112875e2430171fcdbc57cdb69663d6f1
Richard Kasperowski <r@kasperowski.com>
2021-03-22 17:25:19 -0400

    Refactor test code

--- test/cpp_catch2_unittest/GildedRoseCatch2UnitTests.cc
+++ test/cpp_catch2_unittest/GildedRoseCatch2UnitTests.cc
@@ -5,16 +5,18 @@
 TEST_CASE("At the end of each day our system lowers both values (sellin and quality) for every item")
 {
     vector<Item> items;
-    items.push_back(Item("Foo", 0, 0));
-    items.push_back(Item("Bar", 6, 7));
+    items.emplace_back("Foo", 0, 0);
+    items.emplace_back("Bar", 6, 7);
     GildedRose app(items);
     app.updateQuality();
 
-    REQUIRE("Foo" == app.items[0].name);
-    REQUIRE(-1 == app.items[0].sellIn);
-    REQUIRE(0 == app.items[0].quality);
+    Item &item0 = app.items[0];
+    REQUIRE("Foo" == item0.name);
+    REQUIRE(-1 == item0.sellIn);
+    REQUIRE(0 == item0.quality);
 
-    REQUIRE("Bar" == app.items[1].name);
-    REQUIRE(5 == app.items[1].sellIn);
-    REQUIRE(6 == app.items[1].quality);
+    Item &item1 = app.items[1];
+    REQUIRE("Bar" == item1.name);
+    REQUIRE(5 == item1.sellIn);
+    REQUIRE(6 == item1.quality);
 }
