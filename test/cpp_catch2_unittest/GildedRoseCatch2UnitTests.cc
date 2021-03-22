0db1fe6b00b9bb0a12682372b6300cf49c8ccabd
Richard Kasperowski <r@kasperowski.com>
2021-03-22 18:38:10 -0400

    The Quality of an item is never negative

--- test/cpp_catch2_unittest/GildedRoseCatch2UnitTests.cc
+++ test/cpp_catch2_unittest/GildedRoseCatch2UnitTests.cc
@@ -5,15 +5,15 @@
 TEST_CASE("At the end of each day our system lowers both values (sellin and quality) for every item")
 {
     vector<Item> items;
-    items.emplace_back("Foo", 0, 0);
+    items.emplace_back("Foo", 1, 15);
     items.emplace_back("Bar", 6, 7);
     GildedRose app(items);
     app.updateQuality();
 
     Item &item0 = app.items[0];
     REQUIRE("Foo" == item0.name);
-    REQUIRE(-1 == item0.sellIn);
-    REQUIRE(0 == item0.quality);
+    REQUIRE(0 == item0.sellIn);
+    REQUIRE(14 == item0.quality);
 
     Item &item1 = app.items[1];
     REQUIRE("Bar" == item1.name);
@@ -27,11 +27,24 @@ TEST_CASE("Once the sell by date has passed, Quality degrades twice as fast")
     items.emplace_back("Foo", -1, 42);
     GildedRose app(items);
 
-    app.updateQuality();
     Item &item = app.items[0];
+    app.updateQuality();
     REQUIRE(40 == item.quality);
 
     app.updateQuality();
-    item = app.items[0];
     REQUIRE(38 == item.quality);
+}
+
+TEST_CASE("The Quality of an item is never negative")
+{
+    vector<Item> items;
+    items.emplace_back("Foo", 0, 0);
+    GildedRose app(items);
+
+    Item &item = app.items[0];
+    app.updateQuality();
+    REQUIRE(0 == item.quality);
+
+    app.updateQuality();
+    REQUIRE(0 == item.quality);
 }
